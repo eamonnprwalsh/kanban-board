a1dba45d3380e217b6ff598dbe14983582dac578
eamonnprwalsh <132439400+eamonnprwalsh@users.noreply.github.com>
2024-05-20 22:41:05 +0100

    Feature/add storage (#2)
    * few small updates & fix task count
    
    * adding storage

--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -1,26 +1,29 @@
 export const tasksReducer = (tasks, action) => {
+  let updatedTasks = { ...tasks };
+
   if (action.type === 'ADD_TASK') {
-    return {
-      ...tasks,
-      [action.status]: [...tasks[action.status], action.task],
-    };
+    updatedTasks[action.status] = [...tasks[action.status], action.task];
   }
+
   if (action.type === 'MOVE_TASK') {
     const { fromStatus, toStatus, task } = action;
-    return {
-      ...tasks,
-      [fromStatus]: tasks[fromStatus].filter((t) => t.id !== task.id),
-      [toStatus]: [...tasks[toStatus], task],
-    };
+    updatedTasks[fromStatus] = tasks[fromStatus].filter(
+      (t) => t.id !== task.id
+    );
+    updatedTasks[toStatus] = [...tasks[toStatus], task];
   }
 
   if (action.type === 'REMOVE_TASK') {
     const { status, task } = action;
-    return {
-      ...tasks,
-      [status]: tasks[status].filter((_task) => _task.id !== task.id),
-    };
+    updatedTasks[status] = tasks[status].filter(
+      (_task) => _task.id !== task.id
+    );
   }
 
-  return tasks;
+  if (action.type === 'REHYDRATE_TASKS') {
+    updatedTasks = action.tasks;
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  return updatedTasks;
 };
